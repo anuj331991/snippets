7f511903d5d53f8bac862614a3b4bf4b9202020e
Anuj Kumar <anujrajput25@yahoo.com>
2022-01-25 01:07:28 +0530

    added currying nd deep cloning custom function

--- index.js
+++ index.js
@@ -13,3 +13,46 @@ arr.__proto__.yummy = function () {
 };
 console.log(arr.__proto__);
 console.log(Array.prototype);
+
+console.log("===>>>Currrying starts");
+// function volume(length, width, height) {
+//   return length * width * height;
+// }
+function volume(length) {
+  return function (width) {
+    return function (height) {
+      return length * width * height;
+    };
+  };
+}
+console.log("Volume", volume(1)(2)(3));
+console.log("Currrying ends<<<====");
+
+console.log("===>>>Custom deep clone");
+
+let nestedObject = {
+  a: 3,
+  b: {
+    c: {
+      d: 9,
+    },
+  },
+};
+let nested2 = deepClone(nestedObject);
+
+nested2["b"]["c"]["d"] = "anuj";
+console.log(nestedObject);
+
+function deepClone(obj) {
+  let clone = {};
+  for (let key in obj) {
+    if (key != null && typeof obj[key] == "object") {
+      clone[key] = deepClone(obj[key]);
+    } else {
+      clone[key] = obj[key];
+    }
+  }
+  return clone;
+}
+
+console.log("Custom deep clone<<<====");
