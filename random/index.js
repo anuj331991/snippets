ea20a65ec32d49642f44c0d6da2d4a1738595c50
Anuj Kumar <anujrajput25@yahoo.com>
2022-03-01 23:56:03 +0530

    Image optimization

--- random/index.js
+++ random/index.js
@@ -1,50 +1,9 @@
-var a = 1;
-function x() {
-  var a = 2;
-  function y() {
-    console.log(a); //undefnined
-    a++;
-    console.log(a); //NaN
-    a = 8;
-    console.log(a); //8
-    var a = 10;
-  }
-
-  y();
-  console.log(a); //2
+var v;
+function f1() {
+  console.log(v);
 }
-x();
-console.log(a); //1
-
-let obj = {
-  a: "5",
-  function1: function () {
-    console.log(this);
-  },
-  function2: () => {
-    console.log(this);
-  },
-};
-
-obj.function1();
-obj.function2();
-
-var bunny = {
-  name: "Usagi",
-  tasks: ["transform", "eat cake", "blow kisses"],
-  showTasks: function () {
-    this.tasks.forEach(function (task) {
-      alert(this.name + " wants to " + task);
-    });
-  },
-};
-
-const sum = function (a) {
-  return function (b) {
-    if (b) {
-      return sum(a + b); // it takes an argument and return a function which again can take an argument.
-    }
-    return a; // it will keep on adding 1+2+3+4..
-  };
-};
-console.log(sum(1)(2)(3)());
+function f2() {
+  var v = 2;
+  f1();
+}
+f2();
